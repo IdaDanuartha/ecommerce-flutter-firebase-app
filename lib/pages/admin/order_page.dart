f096951c46769e74ebfdaa2d16e2c1abb3fd0f77
IdaDanuartha <danuart21@gmail.com>
2024-05-14 23:42:27 +0800

    Progress : add condition in status order

--- lib/pages/admin/order_page.dart
+++ lib/pages/admin/order_page.dart
@@ -263,24 +263,24 @@ List<TableRow> dataRows(OrderProvider orderProvider, BuildContext context) {
                         color: order.status == 1
                             ? Colors.amber[100]
                             : order.status == 2
-                                ? Colors.orange[100]
-                                : Colors.green[100],
+                                ? Colors.orange[100] : order.status == 3 ? 
+                                 Colors.green[100] : Colors.red[100],
                         borderRadius: BorderRadius.circular(4)),
                     child: Center(
                       child: Text(
                         order.status == 1
                             ? "Preparing Order"
                             : order.status == 2
-                                ? "Out for Delivery"
-                                : "Delivered",
+                                ? "Out for Delivery" : order.status == 3 ?
+                                "Delivered" : "Cancelled",
                         style: primaryTextStyle.copyWith(
                             fontSize: 11,
                             fontWeight: FontWeight.bold,
                             color: order.status == 1
                                 ? Colors.amber[700]
                                 : order.status == 2
-                                    ? Colors.orange[700]
-                                    : Colors.green[700]),
+                                    ? Colors.orange[700] : order.status == 3 ?
+                                    Colors.green[700] : Colors.red[700]),
                       ),
                     ),
                   ),
